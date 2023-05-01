edfe0e17db137c2844311d084a5e12d73868fbfe
MajdFoudeh <majdfoude@gmail.com>
2023-05-02 02:42:41 +0300

    update

--- src/main.js
+++ src/main.js
@@ -36,11 +36,16 @@ const initState = {
 
 }
 
+const data = [];
 
 function Main() {
     const [customername, setcustomername] = useState("");
     const [accountnumber, setaccountnumber] = useState("");
     const [accounttype, setaccounttype] = useState("");
+    const [users, setusers] = useState(data);
+    const [counter, setcounter] = useState(0)
+
+    const handlecounter = (event) => { setcounter(counter + 1) }
 
     const handlechangename = (event) => {
         const name = event.target.value;
@@ -56,20 +61,38 @@ function Main() {
         setaccounttype(accounttype);
     }
 
+    const handleRemove = (id) => {
+        const updateUsers = users.filter((person) => person.counter !== id);
+        setcounter(counter - 1);
+        setusers(updateUsers);
+        // console.log(updateUsers);
+    };
+
     const handlesubmit = (event) => {
-        event.preventfault();
-        return (
-            <>
-                <div className="card" style={{ width: "18rem" }}>
-                    <div className="card-header">Featured</div>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">{customername}</li>
-                        <li className="list-group-item">{accountnumber}</li>
-                        <li className="list-group-item">{accounttype}</li>
-                    </ul>
-                </div>
-            </>
-        )
+        event.preventDefault()
+        console.log("submited");
+        if (!customername) return;
+        const newUser = {
+            counter,
+            customername,
+            accountnumber,
+            accounttype,
+        };
+        const updateUsers = [...users, newUser];
+        setusers(updateUsers);
+        setcustomername("");
+        // return (
+        //     <>
+        //         <div className="card" style={{ width: "18rem" }}>
+        //             <div className="card-header">Featured</div>
+        //             <ul className="list-group list-group-flush">
+        //                 <li className="list-group-item">{customername}</li>
+        //                 <li className="list-group-item">{accountnumber}</li>
+        //                 <li className="list-group-item">{accounttype}</li>
+        //             </ul>
+        //         </div>
+        //     </>
+        // )
     }
     return (
         <>
@@ -83,6 +106,8 @@ function Main() {
                         className="form-control"
                         id="customername"
                         aria-describedby="emailHelp"
+                        value={customername}
+                        onChange={handlechangename}
                     />
                     {/* <div id="emailHelp" className="form-text">
                         We'll never share your email with anyone else.
@@ -92,11 +117,11 @@ function Main() {
                     <label htmlFor="accountnumber" className="form-label">
                         accountNumber
                     </label>
-                    <input type="text" className="form-control" id="accountnumber" />
+                    <input type="text" className="form-control" id="accountnumber" value={accountnumber} onChange={handleaccountnumber} />
                 </div>
                 <div class="mb-3">
                     <label class="form-label text-black">Account Type</label>
-                    <select class="form-select " name="Major" required>
+                    <select class="form-select " name="Major" required value={accounttype} onChange={handleaccounttype}>
 
                         <option>Choose...</option>
                         <option>Savings</option>
@@ -112,13 +137,41 @@ function Main() {
                     <input type="text" className="form-control" id="accounttype" />
                 </div> */}
                 <div className="text-center">
-                    <button type="submit " className="btn btn-primary " onClick={() => { }}>
+                    <button type="submit " className="btn btn-primary " onClick={handlecounter}>
                         Submit
                     </button>
                 </div>
             </form>
+
+            {users.map((user) => {
+                return (
+                    <div className="card" style={{ width: "18rem" }}>
+
+                        <div className="card-body">
+                            <h5 className="card-title">Customer</h5>
+                            <p className="card-text">
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </p>
+                        </div>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">ID: {user.counter}</li>
+                            <li className="list-group-item">Name: {user.customername}</li>
+                            <li className="list-group-item">account number: {user.accountnumber} </li>
+                            <li className="list-group-item">account type: {user.accounttype} </li>
+                        </ul>
+                        <div className="card-body">
+                            <button type="submit" classname="btn btn-danger" onClick={() => handleRemove(user.counter)}>
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+
+                );
+            })}
         </>
     )
+
 }
 
 export default Main
